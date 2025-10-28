cb0799dcd7fe583d1e501696e295c7034876303c
Tea <teawithsuqar@mail.ru>
2025-10-29 01:06:35 +0500

    Забыл импортировать лимитер на поиск

--- app/api/searchUsersThemes/route.ts
+++ app/api/searchUsersThemes/route.ts
@@ -1,5 +1,6 @@
 import {NextResponse} from 'next/server'
 import {prisma} from '@/lib/prisma'
+import {rateLimiterSearch} from '@/proxy'
 
 export async function POST(req:Request){
   const res = await rateLimiterSearch(req)
